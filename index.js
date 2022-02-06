760a3b6806e0880fb458a0fe3222e1eca0a8028f
Ido <ido902@gmail.com>
2022-02-06 18:40:42 +0200

    Auto save last mode

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
+    if(localStorage.getItem("dark") === "true"){
+        $("body").css({"background":"#38434f", "color":"white"});
+        $("#switch").prop("checked", "true");
+    }
    $("#switch").click(function () {
        if($(this).prop("checked")){
            // alert($(".badge-base.LI-profile-badge").attr("data-theme"));
            // $(".badge-base.LI-profile-badge").attr("data-theme", "dark");
            $("body").css({"background":"#38434f", "color":"white"});
+           localStorage.setItem("dark", "true");
        }else{
            $("body").css({"background":"#ffffff", "color":"black"});
+           localStorage.setItem("dark", "false");
            // $(".badge-base.LI-profile-badge").attr("data-theme", "light");
        }
    });
